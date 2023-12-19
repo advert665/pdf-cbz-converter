9a7b3d3ba86068cb5bad9a5f090a5c97add0c980
advert665 <smatthews556@icloud.com>
2023-12-19 18:07:11 +0000

    Final version 0.2

--- pdf-cbz-converter.ps1
+++ pdf-cbz-converter.ps1
@@ -1,7 +1,13 @@
 # pdf-cbz-converter
+# Version 0.2
 # https://github.com/advert665/pdf-cbz-converter/
 
+# import Windows Forms for file dialogue
 Add-Type -AssemblyName System.Windows.Forms
+
+#Constants
+$onlyblackandwhite = "0.00000"
+$version = "0.2"
 
 # Create OpenFileDialog to select a file
 $fileDialog = New-Object System.Windows.Forms.OpenFileDialog
@@ -34,8 +40,39 @@ if ($fileDialog.ShowDialog() -eq 'OK') {
 	  			# Get the full path of the new folder
 	  			$folderPath = $folder.FullName
 
-	  			# Run the code with the input and output parameters
-	  			gswin64c.exe -dNOSAFER -dNOPAUSE -sDEVICE=jpeg -r300 -dJPEGQ=90 -sOutputFile="$folderPath\%02d.jpg" "$fileAddress" -dBATCH
+				# Get colour info from each of the pages using ghostscript's inkcov
+				Write-Output("Analysing page colour data for $filename...")
+				$pagescolourinfo = gswin64c.exe  -o - -sDEVICE=inkcov "$fileAddress" | select-string -Pattern "CMYK"
+				Write-Host("Colour analysis complete.") -ForegroundColor Green
+
+				# Run code based on colour data
+				For($page = 0; $page -le $pagescolourinfo.length-1; $page++) {
+
+					# Convert the colour information into a string and store it
+					$pagecolour = [string]$pagescolourinfo[$page];
+
+					# Paperpage runs one ahead of pagescolour index
+					$paperpage = $page+1; 
+
+					# Test the page colour to see if only black and white
+					if($pagecolour.contains($onlyblackandwhite)){
+
+						# Convert to grayscale png @600dpi, lossless
+						gswin64c.exe -q -dUseCropBox -dNOSAFER -dNOPAUSE -sDEVICE=pnggray -r600 -dFirstPage="$paperpage" -dLastPage="$paperpage" -sOutputFile="$folderPath\$paperpage.jpg" "$fileAddress" -dBATCH
+						Write-Output("Page $paperpage`. Colour values: $pagecolour <<<< black and white")
+					}
+					# Catch all pages that have colour
+					else{
+
+						# Convert to jpeg @400dpi, 90% compression
+						gswin64c.exe -q -dUseCropBox -dNOSAFER -dNOPAUSE -sDEVICE=jpeg -r400 -dJPEGQ=90 -dFirstPage="$paperpage" -dLastPage="$paperpage" -sOutputFile="$folderPath\$paperpage.jpg" "$fileAddress" -dBATCH
+						Write-Output("Converting page $paperpage`. Colour values: $pagecolour <<<< colour")
+					}
+				}
+
+				# Save version information to readme stored in folder
+				$readmepath = Join-Path $folderPath -ChildPath "readme.txt"
+				Write-Output("Converted from pdf with pdf-cbz-converter.ps1 $version.`nPowershell script created by advert665, conversion powered by Ghostscript.`nhttps://github.com/advert665/pdf-cbz-converter/") | Out-File -Filepath $readmepath 
 
 	  			# Compress the new folder into a zip file
 	 			Compress-Archive -Path $folderPath -DestinationPath "$folderPath.zip" -CompressionLevel Optimal -Force
@@ -44,7 +81,11 @@ if ($fileDialog.ShowDialog() -eq 'OK') {
 	  			Rename-Item -Path "$folderPath.zip" -NewName "$folderPath.cbz" -Force
 
 	 			#Remove temporary folder
+				Write-Output("Removing temporary folder...")
 	  			Remove-Item -Path $folderPath -recurse
+
+				#Success message
+				Write-Host("Conversion of $filename to .cbz complete!") -ForegroundColor Green
 			}
 			else {
 				# Display an error message
